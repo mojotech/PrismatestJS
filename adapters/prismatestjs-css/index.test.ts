6a0a09d4e888b89d4f8457273ed41d614c1eb9ac
Daniel Wilson-Thomas <daniel@mojotech.com>
2019-06-30 20:14:26 -0400

    Test the css adapter default views

---
+++ adapters/prismatestjs-css/index.test.ts
@@ -0,0 +1,9 @@
+import { generateTests } from '@mojotech/prismatest-adapter-tests';
+import cssTestView from './index';
+import * as ReactDOM from 'react-dom';
+
+generateTests(cssTestView, e => {
+  const domContainer = document.createElement('div');
+  ReactDOM.render(e, domContainer);
+  return domContainer;
+});
