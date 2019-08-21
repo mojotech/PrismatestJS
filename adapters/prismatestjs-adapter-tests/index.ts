4617025ce4fc37992c9192bbb7c37e6cfa69055f
Daniel Wilson-Thomas <daniel@mojotech.com>
2019-08-21 17:11:49 -0400

    Default view adapter tests for controlled components

---
+++ adapters/prismatestjs-adapter-tests/index.ts
@@ -0,0 +1,14 @@
+import { generateTests as controlledTests } from "./default-test-views-controlled";
+import { generateTests as uncontrolledTests } from "./default-test-views-uncontrolled";
+import * as React from 'react';
+import {
+  TestViewConstructor,
+} from '@mojotech/prismatest';
+
+export const generateTests = <S, E>(
+  adapter: TestViewConstructor<S, E>,
+  render: (e: React.ReactElement) => E
+) => {
+  controlledTests(adapter, render);
+  uncontrolledTests(adapter, render);
+};
